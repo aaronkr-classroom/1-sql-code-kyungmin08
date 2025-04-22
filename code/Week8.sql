f1e13b057aced8cedf0963cd668fce96d08a65a8
kyungmin08 <lmin3905@naver.com>
2025-04-22 10:40:12 +0900

    Add files via upload

---
+++ code/Week8.sql
@@ -0,0 +1,15 @@
+SELECT * FROM 학생2;
+
+--트랜잭션 
+--BEGIN; 또는 BEGIN WORK; 또는 BEGIN TRANSACTION; (postgreSQL)
+BEGIN; --START TRANSACTION;(MY SQL)
+--DELETE FROM 학생 WHERE 학생.성별 ='남';
+DELETE FROM 학생 WHERE 학생.성별 ='여';
+SELECT * FROM 학생;
+ROLLBACK; --저장하고 싶으면 COMMIT;, 취소하고 싶으면 ROLLBACK
+SELECT * FROM 학생;
+
+BEGIN TRANSACTION;
+UPDATE 학생 SET 이름 = '홍길순' WHERE 학번 = 'S002';
+SELECT * FROM 학생;
+COMMIT;
